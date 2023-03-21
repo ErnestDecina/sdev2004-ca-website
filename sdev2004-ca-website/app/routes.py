d288f6f8aee800c31cb5b67e54c6212505248019
Ernest Decina <ernestjohn007@gmail.com>
2023-03-21 22:25:23 +0000

    Init commit

---
+++ sdev2004-ca-website/app/routes.py
@@ -0,0 +1,37 @@
+#
+#   routes.py
+#
+#   Description:
+#       Controls what is given back to client when URL is requested
+#
+#
+#   Authors:
+#       Daniel Wu          (C21460524)
+#       Alex Tsang         (C21751999)
+#       Sean Tighe         (C21329431)
+#       Micheal O'Brien    (C21502889)
+#       Ernest John Decina (C21394933)
+#
+
+# Dependencies
+from app import app
+from flask import render_template
+from flask import session, redirect, request
+from utils import change_lang_code
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    """Landing page users will land at"""
+    return render_template("index.html")
+# End def index()
+
+
+# Set Language
+@app.route('/setlang/<lang_code>')
+def set_language(lang_code):
+    """Change the language code within the users cookies and reserve them the website"""
+    change_lang_code(lang_code)
+    return redirect(request.referrer)
+# End def set_language()
