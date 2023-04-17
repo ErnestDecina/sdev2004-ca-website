8089bf7784de7eee52a03aee0eabbfafbe676b7e
ErnestDecina <ernestjohn007@gmail.com>
2023-04-17 14:02:44 +0100

    Updated main

--- sdev2004-ca-website/main.py
+++ sdev2004-ca-website/main.py
@@ -15,6 +15,8 @@
 
 # Dependencies
 from app import app
+from waitress import serve
 
 # Start Flask Application
-app.run(host='0.0.0.0', port=3000)
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8080)
